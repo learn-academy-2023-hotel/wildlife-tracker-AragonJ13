ed8b51a7e48c9a328c36f4f6621b2c6ff70674a0
AragonJ13 <jeanaragon0@gmail.com>
2023-12-05 16:53:23 -0800

    2nd story complete

---
+++ db/migrate/20231206001518_create_sightings.rb
@@ -0,0 +1,11 @@
+class CreateSightings < ActiveRecord::Migration[7.0]
+  def change
+    create_table :sightings do |t|
+      t.string :latitude
+      t.string :longitude
+      t.string :date
+
+      t.timestamps
+    end
+  end
+end
